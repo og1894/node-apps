12f79452c5bf00cf6d04e18c85a5c99326ba9596
og <20117456pp@gmail.com>
2026-02-12 12:33:00 +0000

    start update

--- controllers/start.js
+++ controllers/start.js
@@ -1,19 +1,16 @@
 'use strict';
-
 import logger from "../utils/logger.js";
 import appStore from "../models/app-store.js";
 
-
 const start = {
+
   createView(request, response) {
-    logger.info("Start page loading!");
-    response.send('Welcome to Oliviers Playlist app!');   
+    const info = appStore.getAppInfo();
+    logger.debug(info);
+    
+    logger.info("Start page loading!")
+    response.json(info);   
   },
 };
-
-const info = appStore.getAppInfo();
-logger.debug(info);
-
-response.json(info); 
 
 export default start;

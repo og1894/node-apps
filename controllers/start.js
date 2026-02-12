6c9ef21b5ec5fe79fab41e9f672235646b9476a6
og <20117456pp@gmail.com>
2026-02-12 12:51:47 +0000

    handlebars and app wont start

--- controllers/start.js
+++ controllers/start.js
@@ -1,15 +1,19 @@
 'use strict';
+
 import logger from "../utils/logger.js";
 import appStore from "../models/app-store.js";
 
 const start = {
-
   createView(request, response) {
-    const info = appStore.getAppInfo();
-    logger.debug(info);
+    logger.info("Start page loading!");
     
-    logger.info("Start page loading!")
-    response.json(info);   
+    const viewData = {
+      title: "Welcome to the Playlist app!",
+      info: appStore.getAppInfo()
+    };
+    
+    //logger.debug(viewData);
+    response.render('start', viewData);   
   },
 };
 

8b75dec4afd1275a4821502bfc8f61fdf770068b
og <20117456pp@gmail.com>
2026-02-26 15:55:14 +0000

    lab more 4

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -4,6 +4,9 @@ import logger from '../utils/logger.js';
 
 const playlist = {
   createView(request, response) {
+    const playlistId = request.params.id;
+    logger.debug(`Playlist id = ${playlistId}`);
+    
     const viewData = {
       title: 'Playlist'
     };
--- models/playlist-store.js
+++ models/playlist-store.js
@@ -13,6 +13,10 @@ const playlistStore = {
     return this.store.findAll(this.collection);
   },
 
+  getPlaylist(id) {
+    return this.store.findOneBy(this.collection, (playlist => playlist.id === id));
+},
+
 };
 
 export default playlistStore;

5396944ff3f47f047b5c497d312e5145b1e72d09
og <20117456pp@gmail.com>
2026-03-24 11:27:30 +0000

    stats 2

--- controllers/stats.js
+++ controllers/stats.js
@@ -9,16 +9,21 @@ const stats = {
     const playlists = playlistStore.getAllPlaylists();
 
     let numPlaylists = playlists.length;
-    
     let numSongs = playlists.reduce((total, playlist) => total + playlist.songs.length, 0);
-	
 	let average =  numPlaylists > 0 ? numSongs/numPlaylists : 0;
-
+    let totalRating = playlists.reduce((total, playlist) => total + parseInt(playlist.rating), 0);
+    let avgRating = numPlaylists > 0 ? totalRating/numPlaylists : 0;
+    let maxRating = Math.max(...playlists.map(playlist => playlist.rating));
+    let maxRated = playlists.filter(playlist => playlist.rating === maxRating);
+    let favTitles = maxRated.map(item => item.title);
 
     const statistics = {
       displayNumPlaylists: numPlaylists,
-      displayNumSongs: numSongs,
-	  displayAverage: average.toFixed(2)
+        displayNumSongs: numSongs,
+        displayAverage: average.toFixed(2),
+        displayAvgRating: avgRating.toFixed(2),
+	    highest: maxRating,
+        displayFav: favTitles
     }
 
     const viewData = {
